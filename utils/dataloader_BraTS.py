06018dd20ea421caacf03f9dc6545e0b75129398
victorshen <1120191066@bit.edu.cn>
2022-04-20 11:44:32 +0800

    添加BraTS数据集处理，未完成

---
+++ utils/dataloader_BraTS.py
@@ -0,0 +1,16 @@
+# todo: add relevant libs
+import os
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from PIL import Image, ImageOps
+
+SEGMENT_CLASS = {
+   0: 'NOT TUMOR',
+   1: 'NECROTIC/CORE',
+   2: 'EDEMA',
+   4: 'ENHANCING'
+}
+
+VOLUME_SLICES = 155
+VOLUME_START_AT = 22
