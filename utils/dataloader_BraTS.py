069d9cca83bafc156346befee423e5ef24e20dfd
victorshen <1120191066@bit.edu.cn>
2022-04-25 15:57:12 +0800

    修改数据读取方式，添加了训练器

--- utils/dataloader_BraTS.py
+++ utils/dataloader_BraTS.py
@@ -1,111 +1,52 @@
-# todo: add relevant libs
-import os
 import numpy as np
-import torch.utils.data as data
-from torch.utils.data import Dataset
-import nibabel as nib
-import torch.nn.functional as F
-from PIL import Image, ImageOps
-from utils.DataAugmentation_BraTS import get_train_transform, get_val_transform
+import cv2 #https://www.jianshu.com/p/f2e88197e81d
+import random
+
+from skimage.io import imread
+from skimage import color
+
+import torch
+import torch.utils.data
+from torchvision import datasets, models, transforms
 
 
-SEGMENT_CLASS = {
-   0: 'NOT TUMOR',
-   1: 'NECROTIC/CORE',
-   2: 'EDEMA',
-   4: 'ENHANCING'
-}
+class Dataset(torch.utils.data.Dataset):
 
-MODALITIES = ['t1', 't1ce', 't2', 'flair']
-
-VOLUME_SLICES = 155
-VOLUME_START_AT = 22
-IMG_SIZE = 128
-
-TRAIN_PATH = "../data/train/"
-VALIDATION_PATH = "../data/val/"
-
-
-def process_f32(img_path):
-    """Set all voxels outside the brain mask to 0"""
-    name = os.path.basename(img_path)
-    images = np.stack([
-        np.array(nib_load(os.path.join(img_path, name) + '_' + i + 'nii.gz'),
-                 dtype='float32', order='C') for i in MODALITIES], -1
-    )
-    mask = images.sum(-1) > 0
-
-    for k in range(len(MODALITIES)):
-        x = images[..., k]
-        y = x[mask]
-
-        lower = np.percentile(y, 0.2)
-        upper = np.percentile(y, 99.8)
-
-        x[mask & x < lower] = lower
-        x[mask & x > upper] = upper
-
-        y = x[mask]
-        x -= y.mean()
-        x /= y.std()
-
-        images[..., k] = x
-
-    return images
-
-
-def nib_load(file_name):
-    if not os.path.exists(file_name):
-        raise FileNotFoundError
-    proxy = nib.load(file_name)
-    data = proxy.get_fdata()
-    proxy.uncache()
-    return data
-
-
-class DataGenerator(Dataset):
-    def __init__(self, data_dir, split='train', case_names=[], transform=None):
-        super(DataGenerator, self).__init__()
-        self.data_dir = data_dir
-        self.split = split
-        self.case_names = case_names
-        self.transform = transform
+    def __init__(self, args, img_paths, mask_paths, aug=False):
+        self.args = args
+        self.img_paths = img_paths
+        self.mask_paths = mask_paths
+        self.aug = aug
 
     def __len__(self):
-        return len(self.case_names)
+        return len(self.img_paths)
 
-    def __getitem__(self, index):
-        name = self.case_names[index]
-        base_dir = os.path.join(self.data_dir, name)
+    def __getitem__(self, idx):
+        img_path = self.img_paths[idx]
+        mask_path = self.mask_paths[idx]
+        npimage = np.load(img_path)
+        npmask = np.load(mask_path)
+        npimage = npimage.transpose((2, 0, 1))
 
-        flair = np.array(nib_load(base_dir + '_flair.nii.gz'), dtype='float32')
-        t1 = np.array(nib_load(base_dir + '_t1.nii.gz'), dtype='float32')
-        t1ce = np.array(nib_load(base_dir + '_t1ce.nii.gz'), dtype='float32')
-        t2 = np.array(nib_load(base_dir + '_t2.nii.gz'), dtype='float32')
-        mask = np.array(nib_load(base_dir + '_seg.nii.gz'), dtype='uint8')
+        WT_Label = npmask.copy()
+        WT_Label[npmask == 1] = 1.
+        WT_Label[npmask == 2] = 1.
+        WT_Label[npmask == 4] = 1.
+        TC_Label = npmask.copy()
+        TC_Label[npmask == 1] = 1.
+        TC_Label[npmask == 2] = 0.
+        TC_Label[npmask == 4] = 1.
+        ET_Label = npmask.copy()
+        ET_Label[npmask == 1] = 0.
+        ET_Label[npmask == 2] = 0.
+        ET_Label[npmask == 4] = 1.
+        nplabel = np.empty((160, 160, 3))
+        nplabel[:, :, 0] = WT_Label
+        nplabel[:, :, 1] = TC_Label
+        nplabel[:, :, 2] = ET_Label
+        nplabel = nplabel.transpose((2, 0, 1))
 
-        if self.split == 'train':
-            item = self.transform({'flair': flair, 't1': t1, 't1ce': t1ce, 't2': t2, 'label': mask})[0]
-        elif self.split == 'val':
-            item = self.transform({'flair': flair, 't1': t1, 't1ce': t1ce, 't2': t2, 'label': mask})
-        else:
-            raise NotImplementedError
+        nplabel = nplabel.astype("float32")
+        npimage = npimage.astype("float32")
 
-        return item['image'], item['label'], index, name
-
-
-def train_loader(data_path, case_names, train_batch_size, num_workers, patch_size=128, pos_ratio=1.0, neg_ratio=1):
-    train_transform = get_train_transform(patch_size, pos_ratio, neg_ratio)
-    train_data = DataGenerator(data_path, 'train', case_names, train_transform)
-
-    return data.DataLoader(train_data, batch_size=train_batch_size, shuffle=True, drop_last=False,
-                           num_workers=num_workers, pin_memory=True)
-
-
-def val_loader(data_path, case_names, val_batch_size, num_workers):
-    val_transform = get_train_transform()
-    val_data = DataGenerator(data_path, 'val', case_names, val_transform)
-
-    return data.DataLoader(val_data, batch_size=val_batch_size, shuffle=True, drop_last=False,
-                           num_workers=num_workers, pin_memory=True)
-
+        return npimage,nplabel
--- utils/loss.py
+++ utils/loss.py
@@ -1,131 +1,38 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import numpy as np
-from torch import Tensor
+
+try:
+    from LovaszSoftmax.pytorch.lovasz_losses import lovasz_hinge
+except ImportError:
+    pass
 
 
-def sum_tensor(inp, axes, keepdim=False):
-    axes = np.unique(axes).astype(int)
-    if keepdim:
-        for ax in axes:
-            inp = inp.sum(int(ax), keepdims=True)
-    else:
-        for ax in sorted(axes, reverse=True):
-            inp = inp.sum(int(ax))
-    return inp
+class BCEDiceLoss(nn.Module):
+    def __init__(self):
+        super(BCEDiceLoss, self).__init__()
+
+    def forward(self, input, target):
+        bce = F.binary_cross_entropy_with_logits(input, target)
+        smooth = 1e-5
+        input = torch.sigmoid(input)
+        num = target.size(0)
+        input = input.view(num, -1)
+        target = target.view(num, -1)
+        intersection = (input * target)
+        dice = (2. * intersection.sum(1) + smooth) / (input.sum(1) + target.sum(1) + smooth)
+        dice = 1 - dice.sum() / num
+        return 0.5 * bce + dice
 
 
-def get_tp_fp_fn_tn(net_output, gt, axes=None, mask=None, square=False):
-    """
-    net_output must be (b, c, x, y(, z)))
-    gt must be a label map (shape (b, 1, x, y(, z)) OR shape (b, x, y(, z))) or one hot encoding (b, c, x, y(, z))
-    if mask is provided it must have shape (b, 1, x, y(, z)))
-    :param net_output:
-    :param gt:
-    :param axes: can be (, ) = no summation
-    :param mask: mask must be 1 for valid pixels and 0 for invalid pixels
-    :param square: if True then fp, tp and fn will be squared before summation
-    :return:
-    """
-    if axes is None:
-        axes = tuple(range(2, len(net_output.size())))
+class LovaszHingeLoss(nn.Module):
+    def __init__(self):
+        super(LovaszHingeLoss, self).__init__()
 
-    shp_x = net_output.shape
-    shp_y = gt.shape
+    def forward(self, input, target):
+        input = input.squeeze(1)
+        target = target.squeeze(1)
+        loss = lovasz_hinge(input, target, per_image=True)
 
-    with torch.no_grad():
-        if len(shp_x) != len(shp_y):  # gt (b, x, y(, z))
-            gt = gt.view((shp_y[0], 1, *shp_y[1:]))  # gt (b, 1, x, y(, z))
-
-        if all([i == j for i, j in zip(net_output.shape, gt.shape)]):
-            # if this is the case then gt is probably already a one hot encoding
-            y_onehot = gt
-        else:
-            gt = gt.long()
-            y_onehot = torch.zeros(shp_x)
-            if net_output.device.type == "cuda":
-                y_onehot = y_onehot.cuda(net_output.device.index)
-            y_onehot.scatter_(1, gt, 1)  # (b, 1, ...) --> (b, c, ...)
-
-    # shape: (b, c, ...)
-    tp = net_output * y_onehot
-    fp = net_output * (1 - y_onehot)
-    fn = (1 - net_output) * y_onehot
-    tn = (1 - net_output) * (1 - y_onehot)
-
-    if mask is not None:
-        tp = torch.stack(tuple(x_i * mask[:, 0] for x_i in torch.unbind(tp, dim=1)), dim=1)
-        fp = torch.stack(tuple(x_i * mask[:, 0] for x_i in torch.unbind(fp, dim=1)), dim=1)
-        fn = torch.stack(tuple(x_i * mask[:, 0] for x_i in torch.unbind(fn, dim=1)), dim=1)
-        tn = torch.stack(tuple(x_i * mask[:, 0] for x_i in torch.unbind(tn, dim=1)), dim=1)
-
-    if square:
-        tp = tp ** 2
-        fp = fp ** 2
-        fn = fn ** 2
-        tn = tn ** 2
-
-    if len(axes) > 0:
-        tp = sum_tensor(tp, axes, keepdim=False)
-        fp = sum_tensor(fp, axes, keepdim=False)
-        fn = sum_tensor(fn, axes, keepdim=False)
-        tn = sum_tensor(tn, axes, keepdim=False)
-
-    return tp, fp, fn, tn
-
-
-class SoftDiceWithLogitsLoss(nn.Module):
-    def __init__(self, nonlinear='sigmoid', smooth=1.0):
-        """
-        """
-        super(SoftDiceWithLogitsLoss, self).__init__()
-        self.smooth = smooth
-        self.nonlinear = nonlinear
-
-    def forward(self, x, y, loss_mask=None):
-        shp_x = x.shape
-
-        axes = list(range(2, len(shp_x)))
-
-        if self.nonlinear == 'sigmoid':
-            x = robust_sigmoid(x)
-        else:
-            raise NotImplementedError(self.nonlinear)
-
-        tp, fp, fn, _ = get_tp_fp_fn_tn(x, y, axes, loss_mask, False)
-
-        nominator = 2 * tp + self.smooth
-        denominator = 2 * tp + fp + fn + self.smooth
-
-        dc = nominator / (denominator + 1e-8)
-
-        dc = dc.mean()
-
-        return 1 - dc
-
-
-class SoftDiceBCEWithLogitsLoss(nn.Module):
-    def __init__(self, dice_smooth=1.0):
-        """Binary Cross Entropy & Soft Dice Loss
-
-        Seperately return BCEWithLogitsloss and Dice loss.
-        BCEWithLogitsloss is more numerically stable than Sigmoid + BCE
-        Args:
-            bce_kwargs (dict):
-            soft_dice_kwargs (dict):
-        """
-        super(SoftDiceBCEWithLogitsLoss, self).__init__()
-
-        self.bce = nn.BCEWithLogitsLoss()
-        self.dsc = SoftDiceWithLogitsLoss(nonlinear='sigmoid', smooth=dice_smooth)
-
-    def forward(self, net_output: Tensor, target: Tensor):
-        """Compute Binary Cross Entropy & Region Dice Loss
-        Args:
-            net_output (Tensor): [B, C, ...]
-            target (Tensor): [B, C, ...]
-        """
-        bce_loss = self.bce(net_output, target)
-        dsc_loss = self.dsc(net_output, target)
-
-        return bce_loss, dsc_loss
+        return loss
